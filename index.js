56cfe56c3f1c5de5b0b71341851bac0d58eb953f
Fabio Picheli <contato@fabiopicheli.com>
2017-02-21 11:37:36 -0300

    fixing port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var options = {
   key: fs.readFileSync('./file.pem'),
   cert: fs.readFileSync('./file.crt')
 };
-var serverPort = 443;
+var serverPort = process.env.PORT || 443;
 var server = https.createServer(options, app);
 var io = require('socket.io')(server);
 
