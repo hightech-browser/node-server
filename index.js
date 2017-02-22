53b10ed66f11cc71a8f99b3bd272bbd793e97129
Fabio Picheli <contato@fabiopicheli.com>
2017-02-22 13:52:27 -0300

    fixing bug

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ io.sockets.on('connection', function (socket) {
     if(!clients[clientHash]){
       clients[clientHash] = {
         'name' : data.name,
-        'socket' : []
+        'sockets' : []
       };
     }
     if(clients[clientHash].sockets.indexOf(socket.id) === -1){
